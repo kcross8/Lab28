daca5817343536b43b2f49a6490b27537437b6fa
unknown <kcross8@gmail.com>
2020-06-04 07:21:24 -0400

    1st commit

---
+++ 601WebAPI_Intro/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _601WebAPI_Intro.Models;
+
+namespace _601WebAPI_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SwapiDAL SP = new SwapiDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Person p = SP.GetPerson("people", 3);
+            Planet c = SP.GetPlanet("planets", 3);
+            Starship s = SP.GetStarship("starships", 3);
+            return View(c);
+        }
+
+        public IActionResult PersonSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PersonSearch(int Id)
+        {
+            //we want to use the DAL to search people by Id 
+            Person p = SP.GetPerson("people", Id);
+            return View(p);
+        }
+        public IActionResult PlanetSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PlanetSearch(int Id)
+        {
+            Planet p = SP.GetPlanet("planets", Id);
+            return View(p);
+        }
+        public IActionResult StarshipSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult StarshipSearch(int Id)
+        {
+            Starship s = SP.GetStarship("starships", Id);
+            return View(s);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 601WebAPI_Intro/Models/SwapiDal.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace _601WebAPI_Intro.Models
+{
+    public class SwapiDAL
+    {
+        public string GetAPIJson(string endpoint, int Id)
+        {
+            string url = $"https://swapi.dev/api/{endpoint}/{Id}/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert the response into something useable 
+            //stream is basically another word for 'file'
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string output = sr.ReadToEnd();
+            return output;
+        }
+        public Person GetPerson(string endpoint, int Id)
+        {
+            string personData = GetAPIJson(endpoint, Id);
+            //need to get nuget package 'Newtonsoft.json' for JsonConvert
+            Person p = JsonConvert.DeserializeObject<Person>(personData);
+
+            return p;
+        }
+        public Planet GetPlanet(string endpoint, int Id)
+        {
+            string planetData = GetAPIJson(endpoint, Id);
+            Planet p = JsonConvert.DeserializeObject<Planet>(planetData);
+
+            return p;
+        }
+        public Starship GetStarship(string endpoint, int Id)
+        {
+            string starshipData = GetAPIJson(endpoint, Id);
+            Starship s = JsonConvert.DeserializeObject<Starship>(starshipData);
+
+            return s;
+        }
+    }
+}
